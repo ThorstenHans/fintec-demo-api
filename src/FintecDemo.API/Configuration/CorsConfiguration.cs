6310411231e814028467c8035454a5cc3aa48dd6
Thorsten Hans <thorsten.hans@gmail.com>
2018-11-24 15:11:10 +0100

    fix: set default values to get rid of json config

--- src/FintecDemo.API/Configuration/CorsConfiguration.cs
+++ src/FintecDemo.API/Configuration/CorsConfiguration.cs
@@ -2,10 +2,10 @@ namespace FintecDemo.API.Configuration
 {
     public class CorsConfiguration
     {
-        public bool Disable { get; set; }
-        public bool AllowAnyHeader { get; set; }
-        public bool AllowAnyOrigin { get; set; }
-        public bool AllowAnyMethod { get; set; }
+        public bool Disable { get; set; } = false;
+        public bool AllowAnyHeader { get; set; } = true;
+        public bool AllowAnyOrigin { get; set; } = true;
+        public bool AllowAnyMethod { get; set; } = true;
         public string[] Origins { get; set; }
         public string[] Headers { get; set; }
         public string[] Methods { get; set; }
